ec558b76ad5cf0d0e62a76c05b2ad0ef9fbc7b9e
Kirsten Lund <kirsten.d.lund@gmail.com>
2020-05-19 11:15:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const search = document.getElementById('search'),
+  submit = document.getElementById('submit'),
+  random = document.getElementById('random'),
+  mealsEl = document.getElementById('meals'),
+  resultHeading = document.getElementById('result-heading'),
+  single_mealEl = document.getElementById('single-meal');
+
+
+
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  // clear single meal
+  single_mealEl.innerHTML = '';
+
+  // get search term
+  const term = search.value;
+
+  // check for empty
+  if(term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      resultHeading.innerHTML = `<h2>Search results for "${term}":</h2>`;
+
+      if(data.meals === null) {
+        resultHeading.innerHTML = `There are no search results. Try again.`
+      } else {
+        mealsEl.innerHTML = data.meals.map(meal => `
+        <div class="meal">
+          <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+          <div class="meal-info" data-mealID="${meal.idMeal}">
+          <h3>${meal.strMeal}</h3>
+          </div>
+        </div>
+        `)
+        .join('');
+      }
+    });
+    //clear search text
+    search.value('');
+  } else {
+    alert('Please enter a search term');
+  }
+
+}
+
+
+
+  // Event listeners
+  submit.addEventListener('submit', searchMeal);
+
+  mealsEl.addEventListener('click', e => {
+    const mealInfo = e.path.find(item => {
+      if(item.classList) {
+        return item.classList.contains('meal-info');
+      } else {
+        return false;
+      }
+    });
+    console.log(mealInfo);
+  });
